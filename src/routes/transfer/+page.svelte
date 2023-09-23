fe8a266e89445c6480c2401859fe803936409bce
helcio <h.andre@giap.pl>
2023-09-23 07:48:41 +0100

    Fix typo on transfer folder

---
+++ src/routes/transfer/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>make transfer</h1>
+</div>
+
+<style>
+</style>
